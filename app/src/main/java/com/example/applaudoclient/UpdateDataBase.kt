f69423203b0004f237b82b343f56f100097f8589
jnefta91 <e.josem91@gmail.com>
2019-12-17 14:00:08 -0600

    ädd class for work action on my Application

---
+++ app/src/main/java/com/example/applaudoclient/UpdateDataBase.kt
@@ -0,0 +1,47 @@
+package com.example.applaudoclient
+
+import android.content.Context
+import androidx.work.*
+import com.example.applaudoclient.api.Api
+import com.example.applaudoclient.db.DatabaseHandler
+import com.example.applaudoclient.model.Post
+import retrofit2.Call
+import java.util.concurrent.TimeUnit
+
+class UpdateDataBase(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
+    private val db = DatabaseHandler(appContext)
+
+    override fun doWork(): Result {
+        val call: Call<List<Post>> = Api.retrofit().getPosts()
+        val result = call.execute()
+        if (result.isSuccessful) {
+            db.deleteAll()
+            result.body().forEach { post ->
+                db.updatePost(Post(post.id, post.userId, post.title, post.body))
+            }
+            return Result.success()
+        }
+        return Result.failure()
+    }
+
+    companion object {
+
+        fun start(context: Context) {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val saveRequest =
+                PeriodicWorkRequestBuilder<UpdateDataBase>(15L, TimeUnit.MINUTES)
+                    .setInitialDelay(1L, TimeUnit.MINUTES)
+                    .setConstraints(constraints)
+                    .build()
+            WorkManager.getInstance(context)
+                .enqueue(saveRequest)
+
+        }
+
+    }
+
+}
